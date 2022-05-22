c14b17605999b4e4274a06ce7e3ffcaa8cb0c77e
gianriso <gianriso97@gmail.com>
2022-05-22 16:10:42 -0400

    build bot new server

--- index.js
+++ index.js
@@ -24,25 +24,26 @@ client.on('message', (message) => {
         "\ttiene un tiempo\t" + os.uptime + 
         " segundos de uso, desde el usuario:\t" + os.userInfo().username);
         
-    }else if (command==="recursos"){
-        var mem= (os.freemem()*9.31)/10000000000 ;
-        var total= (os.totalmem()*9.31)/10000000000 ;
-        mem=parseFloat(mem).toFixed(2);
-        total= parseFloat(total).toFixed(2);
-        message.channel.send("hay una memoria libre  de :\t" +mem+"GB"+ " de un total en: \t"+ total+"GB");
-
-    }else if(command==="ip"){
-
-        getIP((err, ip) => {
-            if (err) {
-                throw err;
-            }
-            //console.log(ip);
-            message.channel.send("la ip externa es: " + ip);
-        });
-       
     }
  
+});
+
+client.on('interactionCreate', async message => {
+	
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+    const command = message.content.slice(prefix.length);
+	if (command === 'paid') {
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('primary')
+					.setLabel('Primary')
+					.setStyle('PRIMARY'),
+			);
+
+		await interaction.reply({ content: 'Pong!', components: [row] });
+	}
 });
 
 client.login(config.BOT_TOKEN);
